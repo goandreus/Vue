51f852331af327487b032a456f233e61efafeb4a
goandreus <andreshtml5@gmail.com>
2019-07-25 00:04:52 -0500

    cicle for objects

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const app = new Vue({
     el: '#app',
     data: {
         titulo:'Hola mundo con vue',
-        heroes: ['wolverine','doctor-x','quicksilver']
+        heroes: ['wolverine','doctor-x','quicksilver'],
+        armas: [
+            {nombre:'adamantium', poder:10},
+            {nombre:'Mente', poder:8},
+            {nombre:'Velocidad', poder:2},
+
+        ]
     }
 })
